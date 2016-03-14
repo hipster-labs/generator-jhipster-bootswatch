6c6741bd8dc23d297f9af5f78bb5de83049f9ebb
Deepu k Sasidharan <d4udts@gmail.com>
2016-03-14 21:24:13 +0800

    updates and fix

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,7 +6,7 @@ var packagejs = require(__dirname + '/../../package.json');
 var cheerio = require('cheerio');
 
 // Stores JHipster variables
-var jhipsterVar = {moduleName: 'entity-audit'};
+var jhipsterVar = {moduleName: 'bootswatch'};
 
 // Stores JHipster functions
 var jhipsterFunc = {};
@@ -65,42 +65,40 @@ module.exports = yeoman.generators.Base.extend({
     writeClientSideFiles : function () {
       // collect files to copy
       var files = [
-        { from: this.webappDir + '/app/components/util/bootswatch/_bootswatch.service.js', to: this.webappDir + '/app/components/util/bootswatch/bootswatch.service.js'},
-        { from: this.webappDir + '/app/components/util/bootswatch/_bootswatch.directive.js', to: this.webappDir + '/app/components/util/bootswatch/bootswatch.directive.js'},
-        { from: this.webappDir + '/app/components/util/bootswatch/_bootswatch.controller.js', to: this.webappDir + '/app/components/util/bootswatch/bootswatch.controller.js'}
+        { from: this.webappDir + '/app/components/bootswatch/_bootswatch.service.js', to: this.webappDir + '/app/components/bootswatch/bootswatch.service.js'},
+        { from: this.webappDir + '/app/components/bootswatch/_bootswatch.directive.js', to: this.webappDir + '/app/components/bootswatch/bootswatch.directive.js'},
+        { from: this.webappDir + '/app/components/bootswatch/_bootswatch.controller.js', to: this.webappDir + '/app/components/bootswatch/bootswatch.controller.js'}
       ];
-      if(this.authenticationType == 'oauth2' || jhipsterVar.authenticationType == 'xauth'){
-        files.push({ from: this.webappDir + '/app/components/interceptor/_bootswatch.interceptor.js', to: this.webappDir + '/app/components/interceptor/bootswatch.interceptor.js'});
+      if(this.authenticationType == 'oauth2' || jhipsterVar.authenticationType == 'jwt'){
+        files.push({ from: this.webappDir + '/app/blocks/interceptor/_bootswatch.interceptor.js', to: this.webappDir + '/app/components/interceptor/bootswatch.interceptor.js'});
       }
       this.copyFiles(files);
     },
 
     updateClientSideFiles : function () {
-      jhipsterFunc.addJavaScriptToIndex('components/util/bootswatch/bootswatch.service.js');
-      jhipsterFunc.addJavaScriptToIndex('components/util/bootswatch/bootswatch.directive.js');
-      jhipsterFunc.addJavaScriptToIndex('components/util/bootswatch/bootswatch.controller.js');
-      if(this.authenticationType == 'oauth2' || jhipsterVar.authenticationType == 'xauth'){
-        jhipsterFunc.addJavaScriptToIndex('components/interceptor/bootswatch.interceptor.js');
+      if(this.authenticationType == 'oauth2' || jhipsterVar.authenticationType == 'jwt'){
         jhipsterFunc.addAngularJsInterceptor('BootswatchInterceptor');
       }
       jhipsterFunc.addTranslationKeyToAllLanguages('bootswatch', {'themeSelector' : 'Theme'}, 'addGlobalTranslationKey', jhipsterVar.enableTranslation);
+      jhipsterFunc.rewriteFile(this.webappDir + '/index.html', '<!-- build:css content/css/main.css -->', '<!-- placeholder link to load bootswatch themes, title holds the current applied theme name-->\n' +
+                  '<link rel="stylesheet" href="" id="bootswatch-css" title="Default">\n');
       var html = this.fs.read(this.webappDir + '/index.html');
       var $ = cheerio.load(html);
-      var head = $('head');
+      /*var head = $('head');
       if(head.find('#bootswatch-css').length == 0) {
         head.append('    <!-- placeholder link to load bootswatch themes, title holds the current applied theme name-->\n' +
                     '        <link rel="stylesheet" href="" id="bootswatch-css" title="Default">\n' +
                     '    ');
-      }
+      }*/
       var footer = $('.footer');
       if(footer.find('#bootswatch-theme-switcher').length == 0){
-        footer.append("    <div ng-controller=\"BootswatchController\" id=\"bootswatch-theme-switcher\" class=\"dropup pull-right\">\n" +
-                      "                    <a class=\"btn btn-default dropdown-toggle\" data-toggle=\"dropdown\">\n" +
+        footer.append("    <div ng-controller=\"BootswatchController\" id=\"bootswatch-theme-switcher\" uib-dropdown class=\"dropup pull-right\">\n" +
+                      "                    <a class=\"btn btn-default dropdown-toggle\"  uib-dropdown-toggle>\n" +
                       "                        <span class=\"glyphicon glyphicon-adjust\"></span>\n" +
                       "                        <span class=\"hidden-tablet\" translate=\"bootswatch.themeSelector\">Theme</span>\n" +
                       "                        <b class=\"caret\"></b>\n" +
                       "                    </a>\n" +
-                      "                    <ul class=\"dropdown-menu\" role=\"menu\">\n" +
+                      "                    <ul class=\"dropdown-menu\" role=\"menu\" uib-dropdown-menu>\n" +
                       "                        <li class=\"theme-link\" ng-repeat=\"theme in themes\">\n" +
                       "                            <a href=\"\" jh-switch-theme=\"theme\">{{theme.name}}</a>\n" +
                       "                        </li>\n" +
@@ -110,5 +108,14 @@ module.exports = yeoman.generators.Base.extend({
       }
       this.fs.write(this.webappDir + '/index.html', $.html());
     }
-  }
+  },
+  install: function () {
+    var injectJsFilesToIndex = function () {
+      this.log('\n' + chalk.bold.green('Running gulp Inject to add javascript to index\n'));
+      this.spawnCommand('gulp', ['inject']);
+    };
+    if (!this.options['skip-install'] && !this.skipClient) {
+      injectJsFilesToIndex.call(this);
+    }
+  },
 });
